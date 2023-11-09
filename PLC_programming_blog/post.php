6930fecb53761fd9d445b65375a93d446c9786c5
Roman Moskvita <moskvita.roman@gmail.com>
2023-11-09 11:12:04 +0100

    template post page created

---
+++ PLC_programming_blog/post.php
@@ -0,0 +1,72 @@
+<?php require_once("SNIPPETS/HEADER.php"); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-9">
+            <h1>Wedrowki</h1>
+            <img id="PostImageBanner" src="http://res.cloudinary.com/dmliyxggm/image/upload/v1511702046/poster_ysydov.jpg" alt="">
+
+            <h3 id="PostSnapshot">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat.
+            </h3>
+
+            <h6>Mniejszy naglowek</h6>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <img class="postArticleImage" src="http://res.cloudinary.com/dmliyxggm/image/upload/v1511708668/subheading_cxcp24.png?w=760&h=427&l=50&t=40" />
+
+            <h4 id="PostLinksHeader">Przydatne linki</h4>
+
+            <ul>
+                <li><a href="#">PLC Automatyka</a></li>
+                <li><a href="#">PLC Automatyka</a></li>
+            </ul>
+        </div>
+
+        <div class="col-lg-3">
+            <h2>Popularne</h2>
+            <hr class="postSlidebarLine" />
+            <div class="row">
+
+
+                <div class="col-6">
+                    <div class="col-lg-12 col-md-6 postPopularContainer">
+                        <img class="postPopularImage" src="Images/Cadr_1.jpg" alt="">
+                        <span>Lorem ipsum</span>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="col-lg-12 col-md-6 postPopularContainer">
+                        <img class="postPopularImage" src="Images/Cadr_1.jpg" alt="">
+                        <span>Lorem ipsum</span>
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="col-lg-12 col-md-6 postPopularContainer">
+                        <img class="postPopularImage" src="Images/Cadr_1.jpg" alt="">
+                        <span>Lorem ipsum</span>
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="col-lg-12 col-md-6 postPopularContainer">
+                        <img class="postPopularImage" src="Images/Cadr_1.jpg" alt="">
+                        <span>Lorem ipsum</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once("SNIPPETS/FOOTER.php"); ?>
